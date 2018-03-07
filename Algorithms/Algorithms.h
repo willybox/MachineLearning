69ba10f601ee9a4e3dbf0559ced37de74c5eda3c
WillyBox <ung.willybox@gmail.com>
2018-03-07 14:02:23 +0100

    Linked DLL to C#

--- Algorithms/Algorithms.h
+++ Algorithms/Algorithms.h
@@ -1,6 +1,1 @@
 #pragma once
-_declspec(dllexport) double frand(double a, double b);
-_declspec(dllexport) int add_to_42(int value_to_add);
-_declspec(dllexport) double* linear_create(double* X, double* Y, int nbPoints);
-_declspec(dllexport) int perceptron(double a, double b, double c, double x1, double x2);
-_declspec(dllexport) void linear_train_classification(double * ptr);
