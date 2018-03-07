c6894437ac96b69722457b61ebd7df8bd528decd
WillyBox <ung.willybox@gmail.com>
2018-03-07 14:30:30 +0100

    DLL corrections + DLL connection for linear_train_classification

--- Algorithms/CSharpConsoleTester/Program.cs
+++ Algorithms/CSharpConsoleTester/Program.cs
@@ -20,39 +20,17 @@ namespace CSharpConsoleTester
             int nbPoints = 3;
             int totalSize = 4 + 3 * nbPoints;
 
-            //IntPtr pntX = Marshal.AllocHGlobal(nbPoints * 2);
-            //Marshal.Copy(new double[] { 1, 2, 5, 6, 7, 8 }, 0, pntX, nbPoints * 2);
-            //Console.WriteLine("pnt X: " + pntX);
-
-            //IntPtr pntY = Marshal.AllocHGlobal(nbPoints * 2);
-            //Marshal.Copy(new double[] { 1, -1, 1 }, 0, pntY, nbPoints);
-            //Console.WriteLine("pnt Y: " + pntY);
             IntPtr a = linear_create(new double[] { 1, 2, 5, 6, 7, 8 }, new double[] { 1, -1, 1 }, nbPoints);
-            //double[] pntRes = new double[4 + nbPoints * 3];
-            //pntRes = linear_create(new double[] { 1, 2, 5, 6, 7, 8 }, new double[] { 1, -1, 1 }, nbPoints);
-            //Console.WriteLine(pntRes);
-
-            //double[] res = new double[4 + 3 * nbPoints];
-            //Marshal.Copy(pntRes, res, 0, 4 + 3 * nbPoints);
-            //Console.Write(res);
-            /*
-            Marshal.FreeHGlobal(pntX);
-            Marshal.FreeHGlobal(pntY);
-            Marshal.FreeHGlobal(pntRes);
-            */
-            Console.WriteLine(a);
-            Console.WriteLine("Freedom");
-
+            
             double[] res = new double[totalSize];
             Marshal.Copy(a, res, 0, totalSize);
             Console.WriteLine(res[1]);
             Console.WriteLine(res[2]);
             Console.WriteLine(res[3]);
 
-            //IntPtr ptnRes = new IntPtr(0);
-            //Marshal.Copy(res, 0, ptnRes, totalSize);
-
+            Console.WriteLine("Linear classification");
             linear_train_classification(res);
+
             Console.WriteLine(res[1]);
             Console.WriteLine(res[2]);
             Console.WriteLine(res[3]);
